08bb488b2ff06a6731ddb699889420cf3b369af4
EvGeniusM <thezhekychan@gmail.com>
2024-05-30 13:33:43 +0500

    Fixed tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -23,11 +23,3 @@ class TestPlayer(unittest.TestCase):
         self.assertTrue(self.player.dead_in_conflict(alien_bullet))
         entity = Alien(Vec2(0, 0), 15)
         self.assertFalse(self.player.dead_in_conflict(entity))
-
-    # @patch('projectiles.player_bullet.PlayerBullet')
-    # def test_shoot(self, mock_player_bullet):
-    #     mock_player_bullet_instance = MagicMock()
-    #     mock_player_bullet.return_value = mock_player_bullet_instance
-    #     bullet = self.player.shoot()
-    #     self.assertEqual(bullet, mock_player_bullet_instance)
-    #     mock_player_bullet.assert_called_once_with(self.position, self.direction, self.speed)
